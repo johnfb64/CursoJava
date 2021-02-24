6deaff302dcecff22414bf7b8e7219e5b73d05ed
johnfb64 <jhonn.ballen@gmail.com>
2021-02-23 20:16:06 -0500

    Programa de constantes de prueba

---
+++ Constantes/src/Pack1/Persona.java
@@ -0,0 +1,33 @@
+package Pack1;
+
+public class Persona {
+	private final String nombre;
+	private int edad;
+	
+	//Constuctor
+	
+	public Persona(String nombre, int edad) {
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("El nombre es: "+ nombre);
+		System.out.println("La edad es " + edad);
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+
+}
